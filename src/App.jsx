946aa5ac964d15e9fe553b3359964a8dd591f6c1
Raymundo Mateos <rayma9829@gmail.com>
2023-03-05 02:51:33 -0600

    [ADD] Proyecto basico en react con Vite, con 3 divs en App y estilos para darles dimensión

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="controls"></div>
+      <div className="game_board"></div>
+      <div className="score"></div>
+    </>
+  )
+}
+
+export default App
