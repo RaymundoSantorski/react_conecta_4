3702dffe527bd32fde70d9cc3095057cfa25aeb9
Raymundo Mateos <rayma9829@gmail.com>
2023-03-07 02:05:51 -0600

    [EDIT] se deja más limpio el componente App al borrar de aqui la logica del juego y usar simplemente el hook useGame

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import { Row } from './components/Row';
-
-const initialBoard = [
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-]
+import { useGame } from './hooks/useGame';
 
 function App() {
 
-  const [board, setBoard] = useState(initialBoard);
-  const [turn, setTurn] = useState(1);
-
-  const toggleTurn = () => {
-    if(turn === 1) setTurn(2);
-    else setTurn(1);
-  }
-
-  const doAMove = (col, idx = 0) => {
-    if(idx > 5) return;
-    if(idx === 0 && board[idx][col] !== 0) return;
-    if(board[idx][col] === 0){
-      let auxBoard = [...board];
-      auxBoard[idx][col] = turn;
-      if(idx > 0) auxBoard[idx - 1][col] = 0;
-      setBoard(auxBoard);
-      setTimeout(() => {
-        doAMove(col, idx + 1);
-      }, 200);
-    }
-    toggleTurn();
-  }
+  const [board, doAMove] = useGame();
 
   return (
     <>
