a7a1471b383a5fafc27d8f792b610badd4e0b40c
Raymundo Mateos <rayma9829@gmail.com>
2023-03-07 01:58:18 -0600

    [ADD] Se actualiza Row para utilizar el componente Piece

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
+import { Piece } from './Piece'
 
 export const Row = ({idx, line, doAMove}) => {
     return (
         <div className='game_board_line'>
           {
             line.map((piece, i) => {
-              if(piece === 0) {
-                return (
-                  <div 
-                    key={`${idx}${i}`} 
-                    className='empty_piece'
-                    onClick={() => doAMove(i)}
-                  ></div>
-                )
-              }else return (
-                  <div 
-                    key={`${idx}${i}`} 
-                    className={`piece_${piece}`}
-                    onClick={() => doAMove(i)}
-                  ></div>
-                )
+              return <Piece 
+                    key={`${idx}${i}`}  
+                    piece={piece} 
+                    doAMove={doAMove} 
+                    i={i} 
+                />
             })
           }
         </div>
