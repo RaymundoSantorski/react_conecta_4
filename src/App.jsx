0b544f60047394161eb643aa652ce38f8978b6c0
Raymundo Mateos <rayma9829@gmail.com>
2023-03-07 01:25:21 -0600

    [ADD] funcion doAMove para que al presionar sobre alguna columna, esta misma 'tire' la pieza hasta llegar al siguiente lugar disponible

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,21 @@ const initialBoard = [
 function App() {
 
   const [board, setBoard] = useState(initialBoard);
+  const [turn, setTurn] = useState(1);
+
+  let doAMove = (col, idx = 0) => {
+    if(idx > 5) return;
+    if(idx === 0 && board[idx][col] !== 0) return;
+    if(board[idx][col] === 0){
+      let auxBoard = [...board];
+      auxBoard[idx][col] = turn;
+      if(idx > 0) auxBoard[idx - 1][col] = 0;
+      setBoard(auxBoard);
+      setTimeout(() => {
+        doAMove(col, idx + 1);
+      }, 300);
+    }
+  }
 
   return (
     <>
@@ -26,15 +41,27 @@ function App() {
                   line.map((piece, i) => {
                     if(piece === 0) {
                       return (
-                        <div key={`${idx}${i}`} className='empty_piece'></div>
+                        <div 
+                          key={`${idx}${i}`} 
+                          className='empty_piece'
+                          onClick={() => doAMove(i)}
+                        ></div>
                       )
                     }else if(piece === 1){
                       return (
-                        <div key={`${idx}${i}`} className="piece_1"></div>
+                        <div 
+                          key={`${idx}${i}`} 
+                          className="piece_1"
+                          onClick={() => doAMove(i)}
+                        ></div>
                       )
                     }else{
                       return (
-                        <div key={`${idx}${i}`} className="piece_2"></div>
+                        <div 
+                          key={`${idx}${i}`} 
+                          className="piece_2"
+                          onClick={() => doAMove(i)}
+                        ></div>
                       )
                     }
                   })
