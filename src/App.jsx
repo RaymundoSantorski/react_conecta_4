b301150b8aaac80303aea79fc53051c5fc13ea5f
Raymundo Mateos <rayma9829@gmail.com>
2023-03-14 03:35:50 -0600

    [EDIT] Se traspasan las partes individuales (Board, Footer y Navbar) de App a sus respectivos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
-import { Row } from './components/Row';
+import { Board } from './components/Board';
+import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
 import { useGame } from './hooks/useGame';
 
 function App() {
@@ -8,47 +10,9 @@ function App() {
 
   return (
     <>
-      <div className="game_board">
-        {
-          board.map((line, idx) => {
-            return <Row
-              key={idx}
-              idx={idx}
-              line={line}
-              doAMove={doAMove}
-            />
-          })
-        }
-      </div>
-      <div className="navbar">
-          <p className='turn_1'>&#9673; <span>{wins[1]}</span></p>
-          <h2 
-            className={turn !== 0 
-              ? `turn_${turn}`
-              : `turn_${winner}`
-            }
-          >
-            {
-              winner !== 0
-              ? `Gana el jugador ${winner}`
-              : `Turno del jugador ${turn}`
-            }
-          </h2>          
-          <p className='turn_2'>&#9673; <span>{wins[2]}</span></p>
-      </div>
-      <div className="footer">
-        <button 
-          className='button button_start'
-          onClick={(e) => {
-            e.preventDefault();
-            restart();
-          }}
-        >Comenzar de nuevo</button>
-        <button 
-          className='button button_clear'
-          onClick={clearWins}
-        >Limpiar marcador</button>
-      </div>
+     <Board board={board} doAMove={doAMove} />
+      <Navbar wins={wins} turn={turn} winner={winner} />
+      <Footer clearWins={clearWins} restart={restart} />
     </>
   )
 }
